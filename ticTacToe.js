844836b9d410c14d317a47a2e47fc56c686ae534
조민수 <chominsu10@gmail.com>
2021-05-02 19:17:12 +0900

    [ADD] markTable

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,18 +5,36 @@ const o = "O";
 const x = "X";
 
 const handleMark = e => {
+  const target = e.path[0];
   if (order) {
-    e.path[0].innerText = o;
+    target.innerText = o;
   } else {
-    e.path[0].innerText = x;
+    target.innerText = x;
   }
   order = !order;
+  markTable(target);
+};
+
+const markTable = target => {
+  for (i = 0; i <= 2; i++) {
+    for (j = 0; j <= 2; j++) {
+      if (tableArray[i][j].location === target.classList.value) {
+        tableArray[i][j].chack = true;
+        tableArray[i][j].order = order;
+      }
+    }
+  }
+  console.log(target.classList.value, tableArray);
 };
 
 const init = () => {
   for (let i = 0; i <= 2; i++) {
     for (let j = 0; j <= 2; j++) {
-      tableArray[i][j] = document.querySelector(`.t${i + 1}-${j + 1}`);
+      tableArray[i][j] = {
+        location: `t${i + 1}-${j + 1}`,
+        chack: false,
+        order: null,
+      };
     }
   }
   console.log(tableArray);
